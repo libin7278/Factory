2474e494fdba73a08f476894dd29a1849c40ef1c
binli <binli@ecarx.com.cn>
2017-03-22 16:48:44 +0800

    自定义View 1

---
+++ 工程模式/app/src/main/java/com/libin/factory/activity/CustomViewActivity.java
@@ -0,0 +1,15 @@
+package com.libin.factory.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.libin.factory.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
---
+++ 工程模式/app/src/main/java/com/libin/factory/widget/CustomImageView.java
@@ -0,0 +1,8 @@
+package com.libin.factory.widget;
+
+/**
+ * Created by doudou on 2017/3/22.
+ */
+
+public class CustomImageView {
+}
